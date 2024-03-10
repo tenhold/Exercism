34afd53dfe335419de4e017c40bf6ccf398f629c
tenhold <tenhold@gmail.com>
2024-03-10 12:44:14 -0500

    update word-count

--- ruby/word-count/word_count.rb
+++ ruby/word-count/word_count.rb
@@ -6,9 +6,6 @@ class Phrase
   end
 
   def word_count
-    words_hash = Hash.new(0)
-    words.each { |w| words_hash[w] += 1 }
-
-    words_hash
+    words.group_by(&:itself).transform_values(&:count)
   end
 end
